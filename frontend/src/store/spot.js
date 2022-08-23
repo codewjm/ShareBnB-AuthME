9ac8386873a47cab2c2526918c24b889d7238bb1
Willard Marett <will.j.marett@gmail.com>
2022-08-22 20:31:47 -0500

    Update spot store

--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -1,13 +1,13 @@
 import { csrfFetch } from './csrf';
 
-// Actions
-const LOAD_ALL_SPOTS = 'spots/getAllSpots';
-const CREATE_SPOT = 'spots/createSpot';
-const LOAD_SPOT = 'spots/getSpot';
-const UPDATE_SPOT = 'spots/updateSpot';
-const DELETE_SPOT = 'spots/deleteSpot';
+// Actions Section - CRUD
+const LOAD_ALL_SPOTS = 'spots/getAllSpots'; // Read
+const CREATE_SPOT = 'spots/createSpot'; // Create
+const LOAD_SPOT = 'spots/getSpot'; // Read
+const UPDATE_SPOT = 'spots/updateSpot'; // Update
+const DELETE_SPOT = 'spots/deleteSpot'; // Delete
 
-//Action Creators
+//Action Creators Section
 const loadAll = (spots) => {
   return {
     type: LOAD_ALL_SPOTS,
@@ -15,50 +15,104 @@ const loadAll = (spots) => {
   };
 };
 
-const create = (newSpot) => {
+const createOne = (newSpotData) => {
   return {
     type: CREATE_SPOT,
-    newSpot
+    newSpotData
   };
 };
 
-const load = (spot) => {
+const loadOne = (spotData) => {
   return {
     type: LOAD_SPOT,
-    spot
+    spotData
   };
 };
 
-const update = (spot) => {
+const updateOne = (spotData) => {
   return {
     type: UPDATE_SPOT,
-    spot
+    spotData
   };
 };
 
-const remove = (spotId) => {
+const removeOne = (spotId) => {
   return {
     type: DELETE_SPOT,
     spotId
-  }
-}
+  };
+};
 
-// Action Creators
-export const getAllSpots = (spots) => async (dispatch) => {
-  const res = await fetch('/api/spots')
 
-  if(res.ok) {
+// Thunks Section
+
+// Gat all spots
+export const getAllSpots = () => async (dispatch) => {
+  const res = await fetch('/api/spots');
+
+  if (res.ok) {
     const spots = await res.json();
-    dispatch(loadAll(spots))
-  }
-}
+    dispatch(loadAll(spots));
+  };
+};
 
 
+// Create a spot
+export const createSpot = (newSpotData) => async (dispatch) => {
+  const res = await csrfFetch('/api/spots', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newSpotData)
+  });
+
+  if (res.ok) {
+    const newSpot = await res.json();
+    dispatch(createOne(newSpot));
+  };
+};
 
 
+// Get one spot by Id
+export const getSpot = (spotId) => async (dispatch) => {
+  const res = await fetch(`/api/spots/${spotId}`);
 
-// Thunks
+  if (res.ok) {
+    const spot = await res.json();
+    dispatch(loadOne(spot));
+  };
+};
 
 
+// Update a spot by Id
+export const updateSpot = (spotId, spotData) => async (dispatch) => {
+  const res = await csrfFetch(`/api/spots/${spotId}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(spotData)
+  });
 
-// Reducer
+  if (res.ok) {
+    const spot = await res.json();
+    dispatch(updateOne(spot));
+  };
+};
+
+// Delete a spot by Id
+export const deleteSpot = (spotId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/spots/${spotId}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (res.ok) {
+    dispatch(removeOne(spotId))
+  };
+};
+
+// Reducer Section
+let initialState = {};
+const spotsReducer = (state = initialState, action) => {
+
+};
+
+export default spotsReducer;

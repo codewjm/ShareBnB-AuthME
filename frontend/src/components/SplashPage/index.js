ce96d050a977f21026a750ed23f77bfed82a9929
Willard Marett <will.j.marett@gmail.com>
2022-08-24 09:18:09 -0500

    Update SplashPage - Need to Create Other Components

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,3 +1,22 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllSpots } from '../../store/spot';
 import './SplashPage.css';
+
+const SplashPage = () => {
+  const dispatch = useDispatch();
+  const allSpots = useSelector((state) => Object.values(state.spots));
+
+  useEffect(() => {
+    dispatch(getAllSpots())
+  }, [dispatch])
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+
+}

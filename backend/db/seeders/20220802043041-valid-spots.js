3df2a067d7a2b478b6846c889ba5df641bf44374
Willard Marett <will.j.marett@gmail.com>
2022-09-19 15:37:59 -0500

    update seeder files

--- backend/db/seeders/20220802043041-valid-spots.js
+++ backend/db/seeders/20220802043041-valid-spots.js
@@ -16,7 +16,7 @@ module.exports = {
         lng: -31.12345,
         name: 'Space Place',
         description: 'We have shiny things and a pool of mercury',
-        price: 500
+        price: 255
       },
       {
         ownerId: 2,
@@ -28,7 +28,7 @@ module.exports = {
         lng: -41.654321,
         name: 'Alien Abode',
         description: 'Charming alien hangout',
-        price: 600
+        price: 155
       },
       {
         ownerId: 3,
@@ -40,7 +40,7 @@ module.exports = {
         lng: -51.654321,
         name: 'Saucer Joy Ride',
         description: 'A secluded dwelling shaped like a flying saucer',
-        price: 700
+        price: 145
       },
       {
         ownerId: 4,
@@ -52,7 +52,7 @@ module.exports = {
         lng: -61.654321,
         name: 'Supernova Pad',
         description: 'A bright orange mansion near Manhattan',
-        price: 800
+        price: 140
       },
       {
         ownerId: 5,
@@ -61,10 +61,94 @@ module.exports = {
         state: 'IL',
         country: 'USA',
         lat: 70.123456,
-        lng: -71.654321,
+        lng: -72.654321,
         name: 'Come Find Us',
         description: 'Located on the 5th floor',
-        price: 900
+        price: 525
+      },
+      {
+        ownerId: 2,
+        address: '632 Green Street',
+        city: 'Mobile',
+        state: 'AL',
+        country: 'USA',
+        lat: 71.123456,
+        lng: -73.654321,
+        name: 'House Amongst Trees',
+        description: 'Located within a forest',
+        price: 550
+      },
+      {
+        ownerId: 3,
+        address: '787 Pier Avenue',
+        city: 'Phoenix',
+        state: 'AZ',
+        country: 'USA',
+        lat: 73.123456,
+        lng: -74.654321,
+        name: 'Hot Spot Phoenix',
+        description: 'Located on 5 acres of private land',
+        price: 177
+      },
+      {
+        ownerId: 4,
+        address: '871 Rapid Street',
+        city: 'Rapid City',
+        state: 'SD',
+        country: 'USA',
+        lat: 74.123456,
+        lng: -75.654321,
+        name: 'Rapid House',
+        description: 'This spot offers all amenities and is two stories',
+        price: 120
+      },
+      {
+        ownerId: 5,
+        address: '509 Drain Avenue',
+        city: 'Auburn',
+        state: 'AL',
+        country: 'USA',
+        lat: 75.123456,
+        lng: -76.654321,
+        name: 'Auburn Spot',
+        description: 'The place is great with a pool',
+        price: 330
+      },
+      {
+        ownerId: 2,
+        address: '3375 Atlanta Avenue',
+        city: 'Atlanta',
+        state: 'GA',
+        country: 'USA',
+        lat: 76.123456,
+        lng: -77.654321,
+        name: 'Cool Spot Atlanta',
+        description: 'A great spot in Atlanta',
+        price: 120
+      },
+      {
+        ownerId: 3,
+        address: '125 Apple Avenue',
+        city: 'Savannah',
+        state: 'GA',
+        country: 'USA',
+        lat: 77.123456,
+        lng: -78.654321,
+        name: 'Savannah House',
+        description: 'A nice breezy spot',
+        price: 420
+      },
+      {
+        ownerId: 4,
+        address: '275 Great Street',
+        city: 'Miami',
+        state: 'FL',
+        country: 'USA',
+        lat: 78.123456,
+        lng: -79.654321,
+        name: 'Beach House Miami',
+        description: 'Located right on the beach',
+        price: 350
       },
     ];
 
@@ -81,6 +165,13 @@ module.exports = {
           'San Francisco',
           'New York',
           'Chicago',
+          'Mobile',
+          'Phoenix',
+          'Rapid City',
+          'Auburn',
+          'Atlanta',
+          'Savannah',
+          'Miami'
         ]
       }
     }, {});
--- backend/db/seeders/20220802043407-valid-images.js
+++ backend/db/seeders/20220802043407-valid-images.js
@@ -39,7 +39,55 @@ module.exports = {
         previewImage: true,
         url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
       },
-
+      {
+        spotId: 6,
+        userId: 2,
+        reviewId: 6,
+        previewImage: true,
+        url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
+      },
+      {
+        spotId: 7,
+        userId: 3,
+        reviewId: 7,
+        previewImage: true,
+        url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
+      },
+      {
+        spotId: 8,
+        userId: 4,
+        reviewId: 8,
+        previewImage: true,
+        url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
+      },
+      {
+        spotId: 9,
+        userId: 5,
+        reviewId: 9,
+        previewImage: true,
+        url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
+      },
+      {
+        spotId: 10,
+        userId: 2,
+        reviewId: 10,
+        previewImage: true,
+        url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
+      },
+      {
+        spotId: 11,
+        userId: 3,
+        reviewId: 11,
+        previewImage: true,
+        url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
+      },
+      {
+        spotId: 12,
+        userId: 4,
+        reviewId: 12,
+        previewImage: true,
+        url: 'https://a0.muscache.com/im/pictures/150c0bed-c97e-4180-98bd-51e049cd3015.jpg',
+      },
     ];
 
     return queryInterface.bulkInsert('Images', validImages);
@@ -48,7 +96,7 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Images', {
-      spotId: { [Op.in]: [2, 3, 4, 5] }
+      spotId: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] }
     }, {});
   }
 };
